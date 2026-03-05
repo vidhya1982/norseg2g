f78847f1045ca8837d71081d8511a2c23cc90c0b
Saniya <me.sagedev@gmail.com>
2026-03-05 15:59:23 +0530

    ui updated

--- app/Livewire/Plans.php
+++ app/Livewire/Plans.php
@@ -7,10 +7,18 @@ use App\Models\Zone;
 class Plans extends Component
 { 
     public $zones;
+    public $is_unlimited;
 
     public function mount()
     {
-        $this->zones = Zone::where('status', 'A',)
+        $this->zones = Zone::where('status', 'A')
+            ->orderBy('position')
+            ->get();
+
+         $this->is_unlimited = Zone::where('status', 'A')
+            ->whereHas('plans', function($q){
+                    $q->where('is_unlimited',1);
+            })
             ->orderBy('position')
             ->get();
     }
